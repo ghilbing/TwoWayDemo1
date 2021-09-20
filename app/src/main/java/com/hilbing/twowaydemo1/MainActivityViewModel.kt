cb282ee83c34a2e8c1133b7c6f846eede2874384
Gretel Hilbing <ghilbing@gmail.com>
2021-09-20 15:21:18 -0700

    Initial commit

---
+++ app/src/main/java/com/hilbing/twowaydemo1/MainActivityViewModel.kt
@@ -0,0 +1,13 @@
+package com.hilbing.twowaydemo1
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel: ViewModel() {
+    val userName = MutableLiveData<String>()
+
+
+    init {
+        userName.value = "Gretel"
+    }
+}
